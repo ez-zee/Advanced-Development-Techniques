f3f1b0e44b25bfd798f21e6876b1844955e59d8c
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-10 20:50:34 +0100

    Review class of Models project(properties, formattd string with custom attributes)

---
+++ K2GUCM_ADT_2023241.Logic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace K2GUCM_ADT_2023241.Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ K2GUCM_ADT_2023241.Models/Review.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace K2GUCM_ADT_2023241.Models
+{
+    [Table("Reviews")] //DBtable for storing class instances
+    public class Review
+    {
+        //properties
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Review_ID { get; set; }
+
+        [Required]
+        public int Review_Movie_ID { get; set; }
+
+
+        [MaxLength(2000)]
+        [ToString]
+        public string Review_Content { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Review_ID} - {Review_Content}\n"; //returns formatted string
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    return (obj as Review).Review_Movie_ID == this.Review_Movie_ID &&
+        //        (obj as Review).Review_ID == this.Review_ID &&
+        //        (obj as Review).Review_Content == this.Review_Content;
+        //}
+    }
+}
