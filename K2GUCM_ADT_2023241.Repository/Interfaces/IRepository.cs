1dcf6d46b5abcdd3affb4ac043cde0db18757b7f
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-10 23:44:49 +0100

    Interface & class for Repostisory(CRUD methods for genetic types, queryable collection, DB context)

---
+++ K2GUCM_ADT_2023241.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace K2GUCM_ADT_2023241.Repository.Interfaces
+{
+    //Declares CRUD methods for items of generic type
+    public interface IRepository<T>
+    {
+        void Create(T item);
+        IQueryable<T> ReadAll(); //Gets all items as queryable collection
+        T Read(int id); //Gets by id
+        void Update(T item);
+        void Delete(int id);
+    }
+}
---
+++ K2GUCM_ADT_2023241.Repository/Repository.cs
@@ -0,0 +1,41 @@
+﻿using K2GUCM_ADT_2023241.Data;
+using K2GUCM_ADT_2023241.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2GUCM_ADT_2023241.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected MovieContext ctx; //DB context
+        public Repository(MovieContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        //CRUD
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item); //adds items of generic type to DB
+            ctx.SaveChanges(); //saves changes to DB
+        }
+
+        public void Delete(int id)//same logic for deleting
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public abstract T Read(int id);
+
+        public IQueryable<T> ReadAll()//gets all items as queryable collection
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract void Update(T item);
+    }
+}
