fa413813b84e7f44611eb13fa3fdf66c57e1651f
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-10 21:26:20 +0100

    Logic projectsintroduction: Cast interface and class (CRUD, dependecy injection)

---
+++ K2GUCM_ADT_2023241.Logic/CastLogic.cs
@@ -0,0 +1,59 @@
+﻿using K2GUCM_ADT_2023241.Logic.Interfaces;
+using K2GUCM_ADT_2023241.Models;
+using K2GUCM_ADT_2023241.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2GUCM_ADT_2023241.Logic
+{
+    public class CastLogic : ICastLogic //implements the interface
+    {
+        private readonly IRepository<Cast> repository;
+
+        public CastLogic(IRepository<Cast> repository) //constructor
+        {
+            this.repository = repository;
+        }
+
+        // CRUD
+        public void Add(Cast cast)
+        {
+            repository.Create(cast);
+        }
+
+        public void Delete(int id)
+        {
+            repository.Delete(id);
+        }
+
+        public IQueryable<Cast> GetAll()
+        {
+            return repository.ReadAll();
+        }
+
+        public Cast GetById(int id)
+        {
+            return repository.Read(id);
+        }
+
+        public void Update(Cast cast)
+        {
+            repository.Update(cast);
+        }
+
+        // Non CRUD
+        public List<Cast> ListAllMovieFromAGivenCast(int id) //LINQ to get a list of movies by id
+        {
+            return repository.ReadAll().Where(x => x.Cast_ID == id).ToList();
+        }
+
+        public List<string> ListAllNicknames() //LINQ group & select to get a list of movies
+        {
+            var result = repository.ReadAll().GroupBy(g => g.Cast_Nick).Select(s => s.Key).ToList();
+            return result;
+        }
+    }
+}
---
+++ K2GUCM_ADT_2023241.Logic/Interfaces/ICastLogic.cs
@@ -0,0 +1,18 @@
+﻿using K2GUCM_ADT_2023241.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace K2GUCM_ADT_2023241.Logic.Interfaces
+{
+    //Declarese methods for Cast class (add, delete, get, update)
+    public interface ICastLogic
+    {
+        void Add(Cast cast);
+        void Delete(int id);
+        IQueryable<Cast> GetAll();
+        Cast GetById(int id);
+        List<Cast> ListAllMovieFromAGivenCast(int id); //gets a list of movies by id
+        List<string> ListAllNicknames(); //same but by nicknames
+        void Update(Cast cast);
+    }
+}
