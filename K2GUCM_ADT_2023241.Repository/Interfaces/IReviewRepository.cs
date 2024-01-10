9b883ef0332abd33d195a12913f7d71733adbfaa
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-10 23:11:06 +0100

    IReviewRepositoryinterface + ReviewRepository class. The same logic as in Cast- and MovieRepository.

---
+++ K2GUCM_ADT_2023241.Repository/Interfaces/IReviewRepository.cs
@@ -0,0 +1,11 @@
+﻿using K2GUCM_ADT_2023241.Models;
+
+namespace K2GUCM_ADT_2023241.Repository.Interfaces
+{
+    //Employs the same features & logic as Cast- & MovieRepository
+    public interface IReviewRepository
+    {
+        Review Read(int id);
+        void Update(Review item);
+    }
+}
---
+++ K2GUCM_ADT_2023241.Repository/ReviewRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using K2GUCM_ADT_2023241.Data;
+using K2GUCM_ADT_2023241.Models;
+using K2GUCM_ADT_2023241.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2GUCM_ADT_2023241.Repository
+{
+    //Employs the same logic & implementation as Cast- &MovieRepository
+    public class ReviewRepository : Repository<Review>, IReviewRepository
+    {
+
+        public ReviewRepository(MovieContext ctx) : base(ctx)
+        {
+        }
+
+        public override Review Read(int id)
+        {
+            return ctx.Reviews.FirstOrDefault(t => t.Review_ID == id);
+        }
+
+        public override void Update(Review item)
+        {
+            var old = Read(item.Review_ID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                try
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
+                catch { }
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
