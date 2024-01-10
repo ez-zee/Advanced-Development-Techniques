421bc014e90439fdd7a1485f4ec94100274f9098
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-10 23:03:15 +0100

    Interface & class for MovieRepository. Employs the same structure, logic & implementation as CastRepostiroy.

---
+++ K2GUCM_ADT_2023241.Repository/Interfaces/IMovieRepository.cs
@@ -0,0 +1,11 @@
+﻿using K2GUCM_ADT_2023241.Models;
+
+namespace K2GUCM_ADT_2023241.Repository.Interfaces
+{
+    //Declares methods for MovieRepository class
+    public interface IMovieRepository
+    {
+        Movie Read(int id); // same as in CastReposirory
+        void Update(Movie item);
+    }
+}
---
+++ K2GUCM_ADT_2023241.Repository/MovieRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using K2GUCM_ADT_2023241.Data;
+using K2GUCM_ADT_2023241.Models;
+using K2GUCM_ADT_2023241.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2GUCM_ADT_2023241.Repository
+{
+    //Structure, logic & implementation are identical to CastRepository class
+    public class MovieRepository : Repository<Movie>, IMovieRepository
+    {
+
+        public MovieRepository(MovieContext ctx) : base(ctx)
+        {
+        }
+
+        public override Movie Read(int id)
+        {
+            return ctx.Movies.FirstOrDefault(t => t.Movie_ID == id);
+        }
+
+        public override void Update(Movie item)
+        {
+            var old = Read(item.Movie_ID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                try
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
+                catch { }
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
