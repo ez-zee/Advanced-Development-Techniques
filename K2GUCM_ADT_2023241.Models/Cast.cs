b49c9e1036af6cda3375e8077ccae637481ad156
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-10 20:04:59 +0100

    Movie properties + formatted string generation with custom attributes

---
+++ K2GUCM_ADT_2023241.Models/Cast.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace K2GUCM_ADT_2023241.Models
+{
+    [Table("Casts")] //database table for storing class instances
+    public class Cast
+    {
+        [Key]//primary key
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Cast_ID { get; set; }
+
+        public int Cast_Movie_ID { get; set; }
+
+        [Required]
+        public string Cast_Name { get; set; }
+
+        public string Cast_Nick { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Cast_ID} - {Cast_Name} - {Cast_Nick}\n";
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    return (obj as Cast).Cast_ID == this.Cast_ID &&
+        //        (obj as Cast).Cast_Movie_ID == this.Cast_Movie_ID &&
+        //        (obj as Cast).Cast_Nick == this.Cast_Nick &&
+        //        (obj as Cast).Cast_Name == this.Cast_Name;
+        //}
+    }
+}
---
+++ K2GUCM_ADT_2023241.Models/Movie.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace K2GUCM_ADT_2023241.Models
+{
+    //attribute for generated movie objs
+    public class ToStringAttribute : Attribute
+    {
+
+    }
+
+    [Table("Movies")] //DB table
+    public class Movie
+    {
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ToString]
+        public int Movie_ID { get; set; }
+
+
+        [Required]
+        [MaxLength(100)]
+        [ToString]
+        public string Movie_Title { get; set; }
+
+
+        [ToString]
+        public int Movie_ReleaseYear { get; set; }
+
+        [ToString]
+        public string Movie_Director { get; set; }
+
+        //[JsonIgnore]
+        //public virtual ICollection<Cast> Casts { get; set; }
+        //[JsonIgnore]
+        //public virtual ICollection<Review> Reviews { get; set; }
+
+        public override string ToString()
+        {
+            string x = "";
+
+            foreach (var item in this.GetType().GetProperties().Where(x => x.GetCustomAttribute<ToStringAttribute>() != null))
+            {
+                x += "   ";
+                x += item.Name + "\t=> ";
+                x += item.GetValue(this);
+                x += "\n";
+            }
+
+            return x;
+        }
+
+    }
+}
