c19cb6e0c465bb0f6f49d7da5e542b27e068f97a
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-11 02:46:10 +0100

    NUnit test project created(Mocking, NUnit.Framework, other directives added)

---
+++ K2GUCM_ADT_2023241.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace K2GUCM_ADT_2023241.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
