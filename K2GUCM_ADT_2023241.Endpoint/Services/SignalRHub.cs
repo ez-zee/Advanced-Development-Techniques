e8f24fb968783cc31e6263ba5b87ee98cad08ac2
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-11 07:26:34 +0100

    SignalRHub class provides server-client communication.

---
+++ K2GUCM_ADT_2023241.Endpoint/Services/SignalRHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace K2GUCM_ADT_2023241.Endpoint.Services
+{
+    public class SignalRHub : Hub //inherits from Hub class & enables server-client communication
+    {
+        public override Task OnConnectedAsync() //is called when client connects the hub
+        {
+            Clients.Caller.SendAsync("Connected", Context.ConnectionId); //successful connection & its id
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception) //same logic when disconnected
+        {
+            Clients.Caller.SendAsync("Disconnected", Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
