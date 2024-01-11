f1690dbf18b6367ea9b357bea68c77e5b474b9c6
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-11 11:21:33 +0100

    Revert "MovieContext classof Daa project + small changes"
    This reverts commit 68146fdbbcf26bad0cd86c13de86b562df8532dd.

--- K2GUCM_ADT_2023241.Endpoint/Program.cs
+++ K2GUCM_ADT_2023241.Endpoint/Program.cs
@@ -2,13 +2,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using K2GUCM_ADT_2023241.Endpoint;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace K2GUCM_ADT_2023241.Endpoint2
+namespace K2GUCM_ADT_2023241.Endpoint
 {
     public class Program
     {
