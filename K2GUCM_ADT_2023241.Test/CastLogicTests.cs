7c6731ac48e7055244e7799a195a6eaa757846d2
Azat Garipov <garipov.azat@stud.uni-obuda.hu>
2024-01-11 03:10:18 +0100

    NUnit test project created (Mocking, NUnit.Framework, other directives added)

---
+++ K2GUCM_ADT_2023241.Test/CastLogicTests.cs
@@ -0,0 +1,29 @@
+using Moq;
+using NUnit.Framework;
+using K2GUCM_ADT_2023241.Logic;
+using K2GUCM_ADT_2023241.Logic.Interfaces;
+using K2GUCM_ADT_2023241.Models;
+using K2GUCM_ADT_2023241.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2GUCM_ADT_2023241.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
